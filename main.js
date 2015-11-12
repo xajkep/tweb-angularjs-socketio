efa0e440e0df3625e36a7fbb805183484bf7f067
Benoît Zuckschwerdt <xajkep@devisland.org>
2015-11-12 18:18:06 +0100

    Ajout de commentaires, nettoyage du code

--- main.js
+++ main.js
@@ -1,28 +1,51 @@
+/**
+ * TWEB - AngularJS et Socket.io en pratique
+ *
+ * @author  Benoît Zuckschwerdt
+ * @date    2015-11-12
+ *
+ * @version 1.0
+ *
+ * Code serveur principal:
+ *  + Gestion des routes
+ *  + Gestion la réception des réponses envoyés par les clients
+ *  + Gestion de l'envoie des données du sondage axu clients
+ */
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+// Tableau contenant les compteurs de réponses.
 var poll = [0,0,0];
 
-var bonjour="HELLO";
-
+// Met le serveur en écoute sur le port spécifié.
 server.listen(1337);
 
+// Permet au code client d'utiliser les dossiers et fichiers
+// contenu dans le dossier public.
 app.use(express.static('public'));
 
+// Envoie le fichier index.html lorsque l'on cherche
+// a atteindre la racine /.
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+// Ecoute
 io.on('connection', function (socket) {
+
+  // Réceptionne les réponses, incrémente le compteur cible,
+  // puis envoie les nouvelles données du sondage.
   socket.on('send', function (i) {
     poll[i]++;
-    console.log(i + " " + poll[i]);
+    console.log(i + " " + poll[i]); //debug
 
     socket.emit('poll', poll);
   });
 
+  // Envoie les données du sondage.
+  // (demandé côté client lors de l'initialisation)
   socket.on('poll-request', function (data) {
     socket.emit('poll', poll);
   });
--- public/ctrl/poll.js
+++ public/ctrl/poll.js
@@ -1,16 +1,27 @@
+/**
+ * TWEB - AngularJS et Socket.io en pratique
+ *
+ * @author  Benoît Zuckschwerdt
+ * @date    2015-11-12
+ *
+ * @version 1.0
+ *
+ * Controlleur du sondage graphique:
+ *  + Initialisation des labels du graphique
+ *  + Met à jours le graphique
+ */
 var pollApp = angular.module('pollApp', ['chart.js']);
+
 pollApp.controller('pollCtrl', function($scope) {
 
-  //$scope.bonjour = "BONJOUR";
-
   $scope.chartLabels = ["yes", "no", "maybe"]
-  //$scope.chartData = [1,2,3];
-
-  //var myBarChart = new Chart(ctx).Bar($scope.chartData, {});
 
   var socket = io.connect('http://localhost:1337');
+
+  // Receptionne les données du sondage
+  // et met a jour le graphique.
   socket.on('poll', function (data) {
-    console.log(data);
+    console.log(data); //debug
 
     $scope.$apply(function () {
       $scope.chartData = data;
